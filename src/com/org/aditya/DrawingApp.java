724ab07bbec15f67e8e6c56865b48e1d38b42766
Aditya Dolas <dolasadi@gmail.com>
2020-01-01 23:43:29 +0530

    BeanFactory1

--- src/com/org/aditya/DrawingApp.java
+++ src/com/org/aditya/DrawingApp.java
@@ -1,11 +1,16 @@
 package com.org.aditya;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
-    public static void main(String[] args){
-        Tringle tringle=new Tringle();
+    public static void main(String[] args) {
+        //Tringle tringle=new Tringle();
+        BeanFactory factory = new XmlBeanFactory
+                (new FileSystemResource("spring.xml"));
+        Tringle tringle=(Tringle) factory.getBean("tringle");
         tringle.draw();
     }
 }
