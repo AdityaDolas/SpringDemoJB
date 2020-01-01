31ca42a07d5f9cb63cdeedce0c789b0d0f8cdcc7
Aditya Dolas <dolasadi@gmail.com>
2020-01-01 23:30:31 +0530

    FC

---
+++ src/com/org/aditya/DrawingApp.java
@@ -0,0 +1,11 @@
+package com.org.aditya;
+
+import org.springframework.beans.factory.BeanFactory;
+
+public class DrawingApp {
+
+    public static void main(String[] args){
+        Tringle tringle=new Tringle();
+        tringle.draw();
+    }
+}
---
+++ src/com/org/aditya/Tringle.java
@@ -0,0 +1,8 @@
+package com.org.aditya;
+
+public class Tringle {
+
+    public void draw(){
+        System.out.println("Triangle drawn.");
+    }
+}
